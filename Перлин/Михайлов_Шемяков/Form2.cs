e8831869e223b85ee9f750e1cb2ca5461fe05073
mrProger <60040129+mrProger@users.noreply.github.com>
2021-04-27 16:05:12 +0600

    Add files via upload

---
+++ Перлин/Михайлов_Шемяков/Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Михайлов_Шемяков
+{
+    public partial class Form2 : Form
+    {
+        string num_pict;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            num_pict = domainUpDown1.Text;
+            pictureBox1.ImageLocation = num_pict + ".jpg";
+
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+            num_pict = domainUpDown1.Text;
+            pictureBox1.ImageLocation = num_pict + ".jpg";
+
+        }
+    }
+}
