cedec2f9ea634a987aa9f65ad06d49f841a094f8
mrProger <60040129+mrProger@users.noreply.github.com>
2021-06-07 17:15:30 +0600

    Add files via upload

---
+++ Хребтов, ПР30/Хребтов, ПР30/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Хребтов__ПР30
+{
+    public partial class Form2 : Form
+    {
+        string num_pict = string.Empty;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            num_pict = domainUpDown1.Text;
+            pictureBox1.ImageLocation = num_pict + ".jpg";
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+            num_pict = domainUpDown1.Text;
+            pictureBox1.ImageLocation = num_pict + ".jpg";
+        }
+    }
+}
